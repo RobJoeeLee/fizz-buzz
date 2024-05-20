b3119906edf2acc241e7a6e1540b71d33456adc8
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2024-05-20 10:49:15 -0400

    wrote the loop that executes fizzbuzz

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hi");
+for (let i = 0; i <= 100; i++) { //Loop from 1 to 100
+    if (i % 3 === 0 && i % 5 === 0) { //Check if "i" is a multiple of both 3 and 5
+        console.log("FizzBuzz");
+    } else if(i % 3 === 0){ //Check if "i" is a multiple of 3
+        console.log("Fizz");
+    } else if(i % 5 === 0){ //Check if "i" is a multiple of 5
+        console.log("Buzz");
+    } else { //Prints the number if none of the above conditons are met
+        console.log(i);
+    }
+
+}
